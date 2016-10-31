4535bc1a4ec3034873ae0a3461d0882e0db108f7
Michael Craig <mcraig@bytelaunch.com>
2016-10-31 16:32:54 -0700

    Add GET todos route

--- server/server.js
+++ server/server.js
@@ -22,6 +22,18 @@ app.post('/todos', (req, res) => {
 
 });
 
+
+app.get('/todos', (req, res) => {
+  // var todo = new Todo();
+
+  Todo.find().then( (todos) => {
+    res.send( {todos} );
+  }, (e) => {
+    res.status(400).send(e);
+  });
+
+});
+
 // app.get('/todos', (req, res) => {
 //
 // });
