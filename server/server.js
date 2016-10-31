252b763225dd31226ef174d5a8f716f9dda5d1a3
Michael Craig <mcraig@bytelaunch.com>
2016-10-31 16:54:10 -0700

    Add queries playground file

--- server/server.js
+++ server/server.js
@@ -34,6 +34,17 @@ app.get('/todos', (req, res) => {
 
 });
 
+app.get('/todos/:id', (req, res) => {
+  res.send(req);
+
+  // Todo.find().then( (todos) => {
+  //   res.send( {todos} );
+  // }, (e) => {
+  //   res.status(400).send(e);
+  // });
+
+});
+
 // app.get('/todos', (req, res) => {
 //
 // });
