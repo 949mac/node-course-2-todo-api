442850a909a5678498f476e30b53a17a2d0ffb2a
Michael Craig <mcraig@bytelaunch.com>
2016-11-02 12:27:01 -0700

    Added PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { ObjectID } = require('mongodb')
+const _ = require('lodash');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -79,6 +80,34 @@ app.delete('/todos/:id', (req, res) => {
       res.status(400).send(e);
   });
 });
+
+app.patch('/todos/:id', (req, res) => {
+
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)) {
+    return res.status(400).send();
+  }
+  
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime(); // unix epoch
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new:true}).then( (todo) => {
+    if (!todo) {
+        return res.status(404).send();
+    }
+    res.send( {todo} );
+
+  }).catch((e) => {
+      return res.status(400).send();
+  });
+
+})
 
 app.listen(port, () => {
   console.log(`Started listening on port`, port);
