da071d3ccc25a366c30b827182ccdc36c86a9460
Michael Craig <mcraig@bytelaunch.com>
2016-11-01 23:20:16 -0700

    Add test cases for GET /todos:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,14 +1,17 @@
 const expect = require('expect');
 const request = require('supertest');
-
+const { ObjectID } = require('mongodb');
 const { app } = require('./../server');
 const { Todo } = require('./../models/todo');
 
 const todos = [{
+  _id: new ObjectID(),
   text: 'first test todo'
 }, {
+  _id: new ObjectID(),
   text: 'second test todo'
 }, {
+  _id: new ObjectID(),
   text: 'third test todo'
 }];
 
@@ -74,6 +77,35 @@ describe('GET /todos', () => {
       .expect((res) => {                 // SuperTest
         expect(res.body.todos.length).toBe(todos.length); // Expect
       })
+      .end(done);
+  })
+});
+
+describe('GET /todos/:id', () => {
+
+  it('should return a todo doc', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {                 // SuperTest
+        expect(res.body.todo.text).toBe(todos[0].text); // Expect
+      })
+      .end(done);
+  })
+
+  it('should return a 400 if todo not found', (done) => {
+    var hexId = new ObjectID().toHexString();
+
+    request(app)
+      .get(`/todos/$(hexId)`)
+      .expect(400)
+      .end(done);
+  })
+
+  it('should return a 400 for non-object ids', (done) => {
+    request(app)
+      .get(`/todos/123`)
+      .expect(400)
       .end(done);
   })
 });
